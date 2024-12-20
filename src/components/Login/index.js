82a88a05364a0bc67e32b00641cf35583c64993e
devi-lakshmi <devi.venki8@gmail.com>
2024-12-20 18:25:01 +0100

    updated code in the login component

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,5 +1,5 @@
-// import React from 'react'
-import { useState, useEffect } from 'react';
+
+import { useState } from 'react';
 
 import { useNavigate } from "react-router-dom";
 import Cookies from 'js-cookie';
@@ -19,56 +19,11 @@ const Login = () => {
 
     const [errorMessage, setErrorMessage] = useState(null);
 
-    // // eslint-disable-next-line no-unused-vars
-    // const [isLoginSuccess, setIsLoginSuccess] = useState(false);
-
-    // const [isAutoLoginAttempted, setIsAutoLoginAttempted] = useState(false);
     const { username, password } = form;
     const jwtToken = Cookies.get("jwt_token");
     console.log("jwtToken", jwtToken)
 
-    // useEffect(() => {
-    //     const autoLogin = async () => {
-    //         if (jwtToken !== undefined) {
-    //             navigate("/");
-    //             return; // Prevent further execution
-    //         }
-    //         console.log("Attempting automatic login...");
-    //         const username = "rahul";
-    //         const password = "rahul@2021";
 
-    //         try {
-    //             const url = "https://apis.ccbp.in/login";
-    //             const options = {
-    //                 method: 'POST',
-    //                 headers: {
-    //                     'Content-Type': 'application/json',
-    //                 },
-    //                 body: JSON.stringify({ username, password }),
-    //             };
-
-    //             const response = await fetch(url, options);
-    //             if (!response.ok) {
-    //                 // Handle non-200 responses
-    //                 const data = await response.json();
-    //                 console.error("Auto-login failed:", data.error_msg || "Unknown error");
-    //                 setErrorMessage(data.error_msg || "Auto-login failed.");
-    //             } else {
-    //                 const data = await response.json();
-    //                 console.log("Auto-login successful:", data);
-    //                 Cookies.set('jwt_token', data.jwt_token, { expires: 30 });
-    //                 navigate("/");
-    //             }
-    //         } catch (error) {
-    //             console.error("Error during auto-login:", error);
-    //             setErrorMessage("Something went wrong during auto-login.");
-    //         }
-
-    //         setIsAutoLoginAttempted(true); // Ensure this state is updated
-    //     };
-
-    //     autoLogin();
-    // }, [jwtToken, navigate]);
 
     if (jwtToken !== undefined) {
         console.log("jwtToken", jwtToken)
@@ -113,11 +68,7 @@ const Login = () => {
 
         <div>
             <div className='login-form-container'>
-                {/* {!isAutoLoginAttempted ? (
-                    <div className="form-login">
-                        <p>Attempting to log in automatically...</p>
-                    </div>
-                ) : ( */}
+
                 <form className='form-login' onSubmit={handleSubmit}>
                     <div>
                         <img
